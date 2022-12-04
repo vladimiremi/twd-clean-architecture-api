cae501e1ac713d0fdc44bcda2c0803fa2b0fc7c6
vladimiremi <vladimiremi2014@gmail.com>
2022-12-04 08:17:51 -0300

    test: validate emails with more than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -11,9 +11,14 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeFalsy()
   })
 
-  test('shoul accept valid email', () => {
+  test('should accept valid email', () => {
     const email = 'any@mail.com'
     expect(Email.validate(email)).toBeTruthy()
+  })
+
+  test('should not accept strings larger than 320 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
   })
 
   test('should not accept local part larger than 64 chars', () => {
