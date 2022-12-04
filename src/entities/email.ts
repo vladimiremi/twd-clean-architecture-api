c77aeb851a6e4e470c070486b71b3f05fddd40dd
vladimiremi <vladimiremi2014@gmail.com>
2022-12-04 09:07:42 -0300

    feat: validate emails with parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -18,6 +18,13 @@ export class Email {
       return false
     }
 
+    const domainParts = domain.split('.')
+    if (domainParts.some(function (part) {
+      return part.length > 63
+    })) {
+      return false
+    }
+
     return true
   }
 }
